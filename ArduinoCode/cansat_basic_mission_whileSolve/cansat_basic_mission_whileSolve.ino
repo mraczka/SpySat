11394aec4a833cd26c1ff4c457905fee4b2203c6
LabPlayGit <68331715+LabPlayGit@users.noreply.github.com>
2021-12-15 16:01:01 +0100

    Code for arduino M0
    arduino code responsible for the base mission operations and GPS functions.

---
+++ ArduinoCode/cansat_basic_mission_whileSolve/cansat_basic_mission_whileSolve.ino
@@ -0,0 +1,117 @@
+#include <Arduino.h>
+#include "wiring_private.h"
+#include <TinyGPS++.h>
+#include <CanSatKit.h>
+#include <cmath>
+using namespace CanSatKit;
+
+
+// create Serial2 on pins 2,3
+// RX-3, TX-2
+Uart Serial2 (&sercom2, 3, 2, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+void SERCOM2_Handler(){Serial2.IrqHandler();}
+
+
+// create radio object
+Radio radio(Pins::Radio::ChipSelect,
+            Pins::Radio::DIO0,
+            433.0,
+            Bandwidth_125000_Hz,
+            SpreadingFactor_9,
+            CodingRate_4_8);
+
+TinyGPSPlus gps; // create GPS object
+Frame frame; // create empty frame
+BMP280 bmpSensor; // create bmp object
+
+const int lm35_pin = A0; //LM35 temperature port
+bool info_led_state = true;
+
+
+// a function that calculates temperature (in *C) from analogRead raw reading
+// see datasheet of LM35 and analogRead Arduino function documentations
+float lm35_raw_to_temperature(int raw) {
+  float voltage = raw * 3.3 / (std::pow(2, 12));
+  float temperature = 100.0 * voltage;
+  return temperature;
+}
+
+void setup() {
+  SerialUSB.begin(115200);
+  Serial2.begin(9600);
+
+  // assign pins the SERCOM functionality
+  pinPeripheral(2, PIO_SERCOM_ALT);
+  pinPeripheral(3, PIO_SERCOM_ALT);
+  
+  pinMode(13, OUTPUT);
+ 
+  radio.begin(); // init radio
+
+  // use begin() function to check if sensor is conneted and is able to communicate
+  // with the CanSat board
+  if(bmpSensor.begin()) {SerialUSB.println("BMP init success!");}
+  else {SerialUSB.println("BMP init failed!");}
+
+  // setOversampling function allows to set resolution of the pressure sensor
+  // possible values of setOversampling:
+  //  1 -- 2.62 Pa
+  //  2 -- 1.31 Pa
+  //  4 -- 0.66 Pa
+  //  8 -- 0.33 Pa
+  // 16 -- 0.16 Pa 
+  bmpSensor.setOversampling(16);
+
+  // set the best resolution of measurement available for analogRead
+  // it's 12 bits for SAM21D-based Arduino boards
+  analogReadResolution(12);
+
+  SerialUSB.println("setup complete!"); //debug message
+}
+
+void loop() {
+
+  // Get GPS sensor data-------------------
+  // check if Serial2 has data available from the GPS, then encode the GPS data. 
+  // When the GPS is done transmiting data, the while stops.
+  while(Serial2.available()){
+    gps.encode(Serial2.read());
+  }
+
+  if(gps.location.isUpdated()){
+    frame.println("Position: ");
+    frame.print("Latitude:");
+    frame.println(gps.location.lat(), 6);
+    frame.print("Longitude:");
+    frame.println(gps.location.lng(), 6);
+  }
+
+  
+  // Get BMP sensor data-----------------------
+  if(bmpSensor.begin()){
+    double T, P;
+    // start measurement, wait for result and save variables
+    bmpSensor.measureTemperatureAndPressure(T, P);
+    frame.print("Pressure = ");
+    frame.print(P, 2);
+    frame.println(" hPa");
+  }
+
+  // Get Analog temperature sensor data------------------
+  // read raw analog value from lm35_pin analog input
+  int raw = analogRead(lm35_pin);
+
+  // use lm35_raw_to_temperature function to calculate temperature
+  float temperature = lm35_raw_to_temperature(raw);
+
+  frame.print("Temperature = ");
+  frame.print(temperature);
+  frame.println(" deg C");
+  
+  // tsransmit frame-----------------------------
+  radio.transmit(frame);
+  SerialUSB.println(frame);
+  
+  frame.clear();
+  delay(1000);
+}
